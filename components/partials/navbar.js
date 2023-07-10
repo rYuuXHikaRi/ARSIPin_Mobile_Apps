f8405d2c8284cfae5c3ce3d429004839bb12240e
rYuuXHikaRi <ryuux.devcode@gmail.com>
2023-07-10 14:12:27 +0700

    Update Navbar
    Active screen mark

--- components/partials/navbar.js
+++ components/partials/navbar.js
@@ -1,1 +1,138 @@
-// Dump File
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    Pressable,
+    Dimensions,
+    ImageBackground
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { SvgXml } from 'react-native-svg';
+
+// Icon
+import { homeActive, homeInActive, arsipActive, arsipInActive, unduhActive, 
+         unduhInActive, userSettingActive, userSettingInActive, userListActive, userListInActive } from "../../assets/img/iconSet";
+
+class Navbar extends Component {
+
+    render() {
+        const curPgStyle = (typeStyle, pageName, value1, value2) => {
+            return (
+                typeStyle === "StyleSheet" ? (
+                    this.props.whichPage === pageName ? (
+                        value1
+                    ) : (
+                        value2
+                    )
+                ) : (
+                    this.props.whichPage === pageName ? (
+                        <SvgXml xml={value1} width="28" height="28" />
+                    )  : (
+                        <SvgXml xml={value2} width="28" height="28"/>
+                    )
+                )
+            )
+        };
+
+
+        const { navigation } = this.props;
+        const screenWidth =  Dimensions.get('window').width;
+        return (
+                <View style={[{width: screenWidth},styles.container, {backgroundColor: "#ffffff"}]}>
+
+                    {/* Arsip */}
+                    <ImageBackground source={
+                                                this.props.whichPage === "arsip" ? require("../../assets/img/navbarBg.png") : 
+                                                                                  require("../../assets/img/navbarBgNone.png")
+                        
+                    }>
+                    <Pressable style={styles.itemNavbar} onPress={() => this.props.navigation.replace('search')}>
+                        {curPgStyle('SVG', 'arsip', arsipActive, arsipInActive)}
+                        <Text style={{fontSize: 12, fontWeight:"400", color:this.props.whichPage === "search" ? "#0E8388" : "#2C3333"}}>arsip</Text>
+                    </Pressable>
+                    </ImageBackground>
+
+                    {/* Unduhan */}
+                    <ImageBackground source={
+                                                this.props.whichPage === "unduhan" ? require("../../assets/img/navbarBg.png") : 
+                                                                                  require("../../assets/img/navbarBgNone.png")
+                        
+                    }>
+                        <Pressable style={styles.itemNavbar} onPress={() => this.props.navigation.replace('ranking')}>
+                            {curPgStyle('SVG', 'unduhan', unduhActive, unduhInActive)}
+                            <Text style={{fontSize: 12, fontWeight:"400", color:this.props.whichPage === "ranking" ? "#0E8388" : "#2C3333"}}>unduhan</Text>
+                        </Pressable>
+                    </ImageBackground>
+
+                    {/* Home */}
+                    <ImageBackground source={
+                                                this.props.whichPage === "home" ? require("../../assets/img/navbarBg.png") : 
+                                                                                  require("../../assets/img/navbarBgNone.png")
+                        
+                    }>
+                        <Pressable style={[styles.itemNavbar]} onPress={() => this.props.navigation.replace('home')}>
+                            {curPgStyle('SVG', 'home', homeActive, homeInActive)}
+                            <Text style={{fontSize: 12, fontWeight:"400", color: this.props.whichPage === "home" ? "#0E8388" : "#2C3333"}}>Home</Text>
+                        </Pressable>
+                    </ImageBackground>
+
+                    {/* UserList */}
+                    <ImageBackground source={
+                                                this.props.whichPage === "userList" ? require("../../assets/img/navbarBg.png") : 
+                                                                                  require("../../assets/img/navbarBgNone.png")
+                        
+                    }>
+                        <Pressable style={styles.itemNavbar} onPress={() => this.props.navigation.replace('setting')}>
+                            {curPgStyle('SVG', 'userList', userListActive, userListInActive)}
+                            <Text style={{fontSize: 12, fontWeight:"400", color: this.props.whichPage === "setting" ? "#0E8388" : "#2C3333"}}>Data Akun</Text>
+                        </Pressable>
+                    </ImageBackground>
+
+                    {/* UserSetting */}
+                    <ImageBackground source={
+                                                this.props.whichPage === "userList" ? require("../../assets/img/navbarBg.png") : 
+                                                                                  require("../../assets/img/navbarBgNone.png")
+                        
+                    }>
+                        <Pressable style={styles.itemNavbar} onPress={() => this.props.navigation.replace('setting')}>
+                            {curPgStyle('SVG', 'userSetting', userSettingActive, userSettingInActive)}
+                            <Text style={{fontSize: 12, fontWeight:"400", color: this.props.whichPage === "setting" ? "#0E8388" : "#2C3333"}}>Saya</Text>
+                        </Pressable>
+                    </ImageBackground>
+
+                </View>
+        );
+    }
+}
+export default function (props) {
+    const navigation = useNavigation();
+    return <Navbar {...props} navigation={navigation} />;
+  };
+
+const styles = StyleSheet.create({
+    container: {
+        height: 60,
+        flexDirection : 'row',
+        alignItems : "center",
+        justifyContent: 'space-around',
+        paddingTop: 8,
+        paddingBottom: 8,
+    },
+    navbarText:{
+        fontStyle: "normal",
+        fontWeight: "700",
+        fontSize: 16,
+        color: "#CBE4DE",
+
+        alignSelf: "center",
+    },
+    itemNavbar:{
+        alignItems: "center",
+        justifyContent: "center",
+
+        width: 60,
+        height: 50,
+
+    },
+});
