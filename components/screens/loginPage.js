cf89dab6f3efe381e6475fd2b6507a61b33a3380
rYuuXHikaRi <ryuux.devcode@gmail.com>
2023-07-14 09:14:15 +0700

    Update loginPage.js
    Done for Front-End loginPage

--- components/screens/loginPage.js
+++ components/screens/loginPage.js
@@ -1,6 +1,7 @@
+import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Dimensions, Image, 
-         ImageBackground, TextInput, Pressable, KeyboardAvoidingView } from 'react-native';
+         ImageBackground, TextInput, Pressable, Keyboard, TouchableWithoutFeedback} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { SvgXml } from 'react-native-svg';
 import { LinearGradient } from 'expo-linear-gradient';
@@ -14,62 +15,82 @@ const screenWidth = Dimensions.get('window').width;
 const logoWidth = 113 * 1.3; const logoHeight = 57 * 1.3;
 
 const LoginPage = ({navigation}) => {
+  const [keyBoardStat, setKeyboardStat] = useState('notshowed');
+
+  useEffect(() => {
+    const keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', () => {
+      setKeyboardStat('notshowed');
+      // Logika atau aksi yang ingin Anda jalankan saat keyboard hilang
+    });
+
+    const keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', () => {
+      setKeyboardStat('showed');
+      // Logika atau aksi yang ingin Anda jalankan saat keyboard hilang
+    });
+
+    return () => {
+      keyboardDidHideListener.remove(); // Membersihkan event listener saat komponen dilepas
+      keyboardDidShowListener.remove();
+    };
+  }, []);
+
+  console.log(keyBoardStat);
   return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View style={{backgroundColor: '#F0E5E5'}}> 
           <ImageBackground source={require('../../assets/img/loginBg_dmp.png')} style={{width: screenWidth, height: '100%'}}> 
             <SvgXml xml={loginBg} width={screenWidth + 0.3} style={{position: 'absolute', top: 0,}}/>
-            <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={styles.container}>
+            <View style={styles.container}>
               <SafeAreaView>
                 <View style={styles.logoContainer}>
                   <Image source={require('../../assets/img/logoGGP_white.png')} style={styles.logo}/>
                 </View>
                 
                 <View style={styles.contentContainer}>
-                  <View style={styles.greetingTextContainer}>
-                    <Text style={styles.greetingText}>Halo,</Text>
-                    <Text style={styles.greetingText}>Selamat Datang.</Text>
-                  </View>
-
-                  <View style={styles.loginFormContainer}>
-                    <GradientText 
-                                  colors={['#A6CE39', '#197B40']} x1={-10} y1={6} x2={15} y2={6} 
-                                  style={styles.loginText}
-                    >Masuk</GradientText>
-
-                    <View style={styles.loginForm}>
-                
-                        <View style={[styles.loginCompShape, styles.loginInput]}>
-                          <TextInput
-                                      placeholder='Nama Pengguna / E-mail'
-                          />
-                        </View>
-
-                        <View style={[styles.loginCompShape, styles.loginInput]}>
-                          <TextInput
-                                        placeholder='Kata Sandi'
-                          />
-                        </View>
-
-                        <Pressable onPress={() => {console.log('Login Button Pressed')}} style={{marginTop: 7}}>
-                          <LinearGradient
-                                          colors={['#90C13B', '#7CB53C', '#378D3F']}
-                                          start={[0, 0.5]}
-                                          end={[1, 0.5]}
-                                          style={[styles.loginCompShape, styles.loginButton]}
-                          >
-                            <Text style={styles.loginButtonText}>Masuk</Text>
-                          </LinearGradient>
-                        </Pressable>
+                    <View style={styles.greetingTextContainer}>
+                      <Text style={styles.greetingText}>Halo,</Text>
+                      <Text style={styles.greetingText}>Selamat Datang.</Text>
                     </View>
 
-                  </View>
+                    <View style={keyBoardStat === 'showed' ? styles.loginFormContainerKBShowed : styles.loginFormContainer}>
+                      <GradientText 
+                                    colors={keyBoardStat === 'showed' ? ['#FFFFFF', '#FFFFFF'] : ['#A6CE39', '#197B40']} x1={-10} y1={6} x2={15} y2={6} 
+                                    style={styles.loginText}
+                      >Masuk</GradientText>
+
+                      <View style={styles.loginForm}>
+                          <View style={[styles.loginCompShape, styles.loginInput]}>
+                            <TextInput
+                                        placeholder='Nama Pengguna / E-mail'
+                            />
+                          </View>
+
+                          <View style={[styles.loginCompShape, styles.loginInput]}>
+                            <TextInput
+                                          placeholder='Kata Sandi'
+                            />
+                          </View>
+
+                          <Pressable onPress={() => {console.log('Login Button Pressed')}} style={{marginTop: 7}}>
+                            <LinearGradient
+                                            colors={['#90C13B', '#7CB53C', '#378D3F']}
+                                            start={[0, 0.5]}
+                                            end={[1, 0.5]}
+                                            style={[styles.loginCompShape, styles.loginButton]}
+                            >
+                              <Text style={styles.loginButtonText}>Masuk</Text>
+                            </LinearGradient>
+                          </Pressable>
+                      </View>
+                    </View>
 
                 </View>
               </SafeAreaView>
-            </KeyboardAvoidingView>
+            </View>
           </ImageBackground>
           <StatusBar style="light" />
         </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -107,6 +128,9 @@ const styles = StyleSheet.create({
   },
   loginFormContainer: {
     marginTop: 336
+  },
+  loginFormContainerKBShowed: {
+    marginTop: 40,
   },
   loginText: {
     fontSize: 20,
