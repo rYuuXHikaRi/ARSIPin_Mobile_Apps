bd2dfce633a1048dd48e03774195d1be4974d3af
Ferli <ferli.120140018@student.itera.ac.id>
2023-07-18 09:38:08 +0700

    Update manajemenBerkas.js

--- components/screens/manajemenBerkas.js
+++ components/screens/manajemenBerkas.js
@@ -29,83 +29,95 @@ const ManajemenBerkas = ({ navigation }) => {
   const [namafile, setnamafile] = useState("");
   const [tableData, setTableData] = useState([]);
   const [users, setUsers] = useState([]);
-  const [rowData,setRowData]= useState([]);
-  const [NamaDokumen, setNamaDokumen] = useState('');
-  const [Keterangan, setKeterangan] = useState('');
-  const [Tahun, setTahun] = useState('');
-  const [NamaDesa, setNamaDesa] = useState('');
-  const [LokasiPenyimpanan, setLokasiPenyimpanan] = useState('');
-  const [NamaFile, setNamaFile] = useState('');
-  
-  
+  const [rowData, setRowData] = useState([]);
+  const [modalOpsVisible, setModalOpsVisible] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const options = ["Option 1", "Option 2", "Option 3"];
+
+  const handleSelectOption = (option) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+  };
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+    renderOpsiIcons();
+  };
 
   useEffect(() => {
-    const fetchUsers = async () => {
-
-      try {
-          const response = await fetch('http://192.168.26.249:8000/api/arsips');
-          const data = await response.json();  
-          setUsers(data);
-          tableData.splice(0,tableData.length);
-  
-      } catch (error) {
-          console.log(error);
-      }
-
-
-  };
     fetchUsers();
-    addUsers();
   }, []);
 
+  const fetchUsers = async () => {
+    try {
+      const response = await fetch("http://192.168.67.213:8000/api/arsips");
+      const data = await response.json();
+      setUsers(data);
+      tableData.splice(0, tableData.length);
 
-
-
-const addUsers = () => {
-  users.map((user) => (
-    tableData.push([<TouchableOpacity onPress={() => navigation.navigate('detailberkas',{user})}>
-    <Text key={user.id} style={[styles.tableText, { fontSize: 20 }]}>{user.NamaDokumen}</Text>
-    </TouchableOpacity>,renderOpsiIcons()])
- ))
-
-}
-
-const handleCreate = async () => {
- 
-  const data = {
-    NamaDokumen: NamaDokumen,
-    Keterangan: Keterangan,
-    Tahun: Tahun,
-    NamaDesa: NamaDesa,
-    LokasiPenyimpanan: LokasiPenyimpanan,
-    NamaFile : "File.txt"
+      users.map((user) =>
+        tableData.push([
+          <TouchableOpacity
+            onPress={() => navigation.navigate("detailberkas", { user })}
+          >
+            <Text key={user.id} style={[styles.tableText, { fontSize: 20 }]}>
+              {user.NamaDokumen}
+            </Text>
+          </TouchableOpacity>,
+          // renderOpsiIcons()
+          <View style={styles.opsiContainer}>
+          <TouchableOpacity
+            onPress={() => [[{ toggleDropdown }, console.log("Opsi titik-tiga")]]}
+          >
+            <MaterialCommunityIcons
+              name="dots-vertical"
+              size={30}
+              color="black"
+            />
+          </TouchableOpacity>
+        </View>
+          ,
+        ])
+      );
+      console.log(users);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  try {
-    console.log(data)
-    const response = await axios.post('http://192.168.26.249:8000/api/store', data);
-    console.log('Data created successfully:', response.data);
-    // Reset input fields if needed
-    console.log('berhasil')
-    setModalVisible(false);
+  const tableHead = ["Nama Folder", "Aksi"];
+  const UserList = () => {
+    const addUsers = () => {};
+  };
 
-  } catch (error) {
-    console.error('Error creating data:', error);
-  }
-};
-
-
-
-  
-  const tableHead = ['Nama Folder', 'Aksi'];
-
-
-  function renderOpsiIcons() {
-    return (
-      <View style={styles.opsiContainer}>
-        <TouchableOpacity onPress={() => console.log('Opsi titik-tiga')}>
-          <MaterialCommunityIcons name="dots-vertical" size={30} color="black" />
-        </TouchableOpacity>
+  // function renderOpsiIcons() {
+  //   return (
+  //     <View style={styles.opsiContainer}>
+  //       <TouchableOpacity
+  //         onPress={() => [[{ toggleDropdown }, console.log("Opsi titik-tiga")]]}
+  //       >
+  //         <MaterialCommunityIcons
+  //           name="dots-vertical"
+  //           size={30}
+  //           color="black"
+  //         />
+  //       </TouchableOpacity>
+  //     </View>
+  //   );
+  // }
+  function renderOpsiIcons(){
+    {isOpen && (
+      <View style={styles.dropdownContainer}>
+        {options.map((option) => (
+          <TouchableOpacity
+            key={option}
+            onPress={() => handleSelectOption(option)}
+          >
+            <Text style={styles.dropdownOption}>{option}</Text>
+          </TouchableOpacity>
+        ))}
       </View>
     )}
 
@@ -157,8 +169,7 @@ const handleCreate = async () => {
                 <TextInput
                   style={[styles.input]}
                   placeholder="Nama Dokumen"
-                  value={NamaDokumen}
-                  onChangeText={(text) => setNamaDokumen(text)}
+                  onChangeText={(text) => setnamadokumen(text)}
                 />
               </View>
 
@@ -167,8 +178,7 @@ const handleCreate = async () => {
                 <TextInput
                   style={styles.inputketerangan}
                   placeholder="Keterangan"
-                  value={Keterangan}
-                  onChangeText={(text) => setKeterangan(text)}
+                  onChangeText={(text) => setketerangan(text)}
                 />
               </View>
             </View>
@@ -176,16 +186,14 @@ const handleCreate = async () => {
             <TextInput
               style={styles.input}
               placeholder="tahun"
-              value={Tahun}
-              onChangeText={(text) => setTahun(text)}
+              onChangeText={(text) => settahun(text)}
             />
 
             <Text style={styles.titleform}>Nama Desa</Text>
             <TextInput
               style={styles.input}
               placeholder="Nama Desa"
-              value={NamaDesa}
-              onChangeText={(text) => setNamaDesa(text)}
+              onChangeText={(text) => setnamadesa(text)}
             />
 
             <View>
@@ -194,8 +202,7 @@ const handleCreate = async () => {
                 <TextInput
                   style={styles.input}
                   placeholder="Loker"
-                  value={LokasiPenyimpanan}
-                  onChangeText={(text) => setLokasiPenyimpanan(text)}
+                  onChangeText={(text) => settahun(text)}
                 />
               </View>
 
@@ -204,8 +211,6 @@ const handleCreate = async () => {
                 <TextInput
                   style={styles.inputFile}
                   placeholder="Pilih"
-                  value={NamaFile}
-                  onChangeText={(text) => setNamaFile(text)}
                 ></TextInput>
               </View>
             </View>
@@ -213,7 +218,7 @@ const handleCreate = async () => {
             <View style={styles.btnsave}>
               <TouchableOpacity
                 style={[styles.button, styles.buttonSave]}
-                onPress={handleCreate}
+                onPress={handleSave}
               >
                 <Text style={styles.textStyle}>Simpan</Text>
               </TouchableOpacity>
@@ -239,12 +244,8 @@ const handleCreate = async () => {
           <Text style={[styles.cardTitle2, styles.bottomLine]}>Data Arsip</Text>
         </View>
         <ScrollView>
-        <Table borderStyle={{ borderWidth: 1, borderColor: 'white' }}>
-        
-          <Row data={tableHead} flexArr={[4, 1]} style={[styles.header, styles.boldText]} textStyle={[styles.text, styles.boldText, { fontSize: 20 }]} />
-       
-          
-          {tableData.map((rowData, index,) => (
+          <Table borderStyle={{ borderWidth: 1, borderColor: "white" }}>
+            <UserList />
             <Row
               data={tableHead}
               flexArr={[4, 1]}
