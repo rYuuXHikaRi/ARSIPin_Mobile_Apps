f95fcc9d5c85bb2fb8b4fda015e28a88724f83d8
rYuuXHikaRi <ryuux.devcode@gmail.com>
2023-07-25 15:23:07 +0700

    Create apiEndpoint.js
    Add apiEndpoint.js to make easy change API Endpoint URL

---
+++ components/middleware/apiEndpoint.js
@@ -0,0 +1,4 @@
+export const apiEndpoint = 'http://192.168.5.34:8000';
+export const dataArsipsApi = apiEndpoint + '/api/arsips';
+export const dataUsersApi = apiEndpoint + '/api/users';
+export const storeArsip = apiEndpoint + '/api/store';
