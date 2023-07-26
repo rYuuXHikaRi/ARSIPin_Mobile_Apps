17d127ebbce9c822d4995453bf73dd1b956206e8
rYuuXHikaRi <ryuux.devcode@gmail.com>
2023-07-26 14:52:01 +0700

    Update apiEndpoint.js

--- components/middleware/apiEndpoint.js
+++ components/middleware/apiEndpoint.js
@@ -1,4 +1,4 @@
-export const apiEndpoint = 'http://192.168.6.34:8000';
+export const apiEndpoint = 'http://192.168.89.34:8000';
 export const dataArsipsApi = apiEndpoint + '/api/arsips';
 export const dataUsersApi = apiEndpoint + '/api/users';
 export const storeArsip = apiEndpoint + '/api/store';
