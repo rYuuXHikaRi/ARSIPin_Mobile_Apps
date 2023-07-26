bb262f3cc6103c7af31d5c0b52b754a0a4f2c42b
rYuuXHikaRi <ryuux.devcode@gmail.com>
2023-07-26 09:34:34 +0700

    Update apiEndpoint.js
    add enpoint for list document

--- components/middleware/apiEndpoint.js
+++ components/middleware/apiEndpoint.js
@@ -2,3 +2,4 @@ export const apiEndpoint = 'http://192.168.6.34:8000';
 export const dataArsipsApi = apiEndpoint + '/api/arsips';
 export const dataUsersApi = apiEndpoint + '/api/users';
 export const storeArsip = apiEndpoint + '/api/store';
+export const getListDocApi = apiEndpoint + '/api/getfiles'
