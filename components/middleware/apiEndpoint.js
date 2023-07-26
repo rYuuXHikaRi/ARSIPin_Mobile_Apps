e951efc88fd31c57092edeb177b6852e103a2944
rYuuXHikaRi <ryuux.devcode@gmail.com>
2023-07-26 09:58:09 +0700

    Update apiEndpoint.js
    Add endpoint for getListDocApi, storeUser, destroyUser

--- components/middleware/apiEndpoint.js
+++ components/middleware/apiEndpoint.js
@@ -2,4 +2,6 @@ export const apiEndpoint = 'http://192.168.6.34:8000';
 export const dataArsipsApi = apiEndpoint + '/api/arsips';
 export const dataUsersApi = apiEndpoint + '/api/users';
 export const storeArsip = apiEndpoint + '/api/store';
-export const getListDocApi = apiEndpoint + '/api/getfiles'
+export const getListDocApi = apiEndpoint + '/api/getfiles';
+export const storeUser = apiEndpoint + '/api/users/store';
+export const destroyUser = apiEndpoint + '/api/users/destroy'
