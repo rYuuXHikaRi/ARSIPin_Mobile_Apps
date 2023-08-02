be25f64aba8fbc6f3f6f2d12dfa1d378caa390ab
rYuuXHikaRi <ryuux.devcode@gmail.com>
2023-08-02 14:07:45 +0700

    Delete AndroidSafeView.js

--- components/AndroidSafeView.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet, Platform, StatusBar } from "react-native";
-
-export default StyleSheet.create({
-  AndroidSafeArea: {
-    flex: 1, 
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    backgroundColor: "#2C3333"
-  }
-});
