c7c1188410674b08f262ff3d71e1f3e235866a0c
rYuuXHikaRi <ryuux.devcode@gmail.com>
2023-08-03 14:53:19 +0700

    Update: Beautify Code
    Move styling to Stylesheet

--- components/partials/popUpMenu/popUpMenu.js
+++ components/partials/popUpMenu/popUpMenu.js
@@ -7,8 +7,10 @@ import {
 } from "react-native-popup-menu";
 
 import {  MaterialCommunityIcons } from "@expo/vector-icons";
+import { useSelector } from "react-redux";
 
 export default function PopUpMenu({children}) {
+    
     return (
         <Menu >
           <MenuTrigger >
@@ -18,11 +20,26 @@ export default function PopUpMenu({children}) {
                color="black"
              />
           </MenuTrigger>
-          <MenuOptions style={{marginTop: -157, marginLeft: 60}}>
-           <View style={{backgroundColor: 'white', flex:1, flexDirection: 'row', width: 130, height: 40, justifyContent: "space-between", alignItems: 'center'}}>
+          <MenuOptions style={[styles.container]}>
+           <View style={[styles.layout]}>
             {children}
            </View>
           </MenuOptions>
         </Menu>
     );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: -177,
+    marginLeft: 60
+  },
+  layout: {
+    backgroundColor: 'white', 
+    flex:1, 
+    flexDirection: 'row', 
+    width: 130, height: 40, 
+    justifyContent: "space-between", 
+    alignItems: 'center'
+  }
+});
