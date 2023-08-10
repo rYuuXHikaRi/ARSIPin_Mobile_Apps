2af6ee775a06b117bf476139184536da5def4be5
rYuuXHikaRi <ryuux.devcode@gmail.com>
2023-08-10 08:40:58 +0700

    Update modalEditDoc.js

--- components/partials/modals/modalEditDoc.js
+++ components/partials/modals/modalEditDoc.js
@@ -1,6 +1,6 @@
 import { Modal, View, Text, Pressable, StyleSheet } from 'react-native';
 
-export default function ModalEditDoc({ isVisible, children }) {
+export default function ModalEditDoc({ isVisible, children, }) {
     return (
     <Modal animationType="slide" transparent={true} visible={isVisible}>
           {children}
